3d78b0d32478f481792f76549f2005d322c3508b
root <root@localhost.localdomain>
2021-04-24 12:11:56 +0300

    update recom.

--- globs.py
+++ globs.py
@@ -17,11 +17,14 @@ def init():
 
     # Some explanations for these recommendations can be found in sheet attached to this project, but here are some:
     # TP-link default pass attacks - router may have default 8-digit password
-    # After some experience with these routers (vendors), many of them are vulnerable to pixie dust
+    # After some experience with ralink routers (vendors), many of them are vulnerable to pixie dust
     # After some research, NETGEAR routers seem to have a pattern for default passwords :D
-    recommended_vendors = {"ralink": "Pixie Dust", "realtek": "Pixie Dust"}
+    recommended_vendors = {"ralink": "Pixie Dust", "realtek": "Pixie Dust", \
+                            "rtl8192ce": "Pixie Dust", "rtl8192er": "Pixie Dust", "rtl8812ar": "Pixie Dust", \
+                            "rt": "Pixie Dust", 'mt': "Pixie Dust"}
     recommended_names = {'tp_link': "8-digit Dictionary", 'tp-link': "8-digit Dictionary", \
-                            "netgear": 'adjective + noun + 3 digits'}
+                            "netgear": 'adjective + noun + 3 digits', 'alfa': 'Pixie Dust', 'huawei': "Pixie Dust", \
+                            "linksys": "Pixie Dust" }
     
     # Will define these in main app
     spoof_option = None 
